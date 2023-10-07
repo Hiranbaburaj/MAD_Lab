81d7d8e4ff94c18320457d89e7a360cdf79b0f15
hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-10-07 12:00:54 +0530

    Update MainActivity.java

--- Qn3/MainActivity.java
+++ Qn3/MainActivity.java
@@ -1,8 +1,11 @@
-package com.example.test;
+package com.example.qn3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clickme1 = findViewById(R.id.submit);
+        clickme1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
