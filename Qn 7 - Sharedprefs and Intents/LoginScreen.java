e201ccbd9b343c6cab36533e98f991f4acf20ee4
hiranbaburaj <116332096+Hiranbaburaj@users.noreply.github.com>
2023-12-06 22:43:26 +0530

    Code Fix

---
+++ Qn 7 - Sharedprefs and Intents/LoginScreen.java
@@ -0,0 +1,24 @@
+package com.example.qn7_sharedprefsandintents;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen);
+    }
+    public void login(View view) {SharedPreferences preferences = getSharedPreferences("creds", MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        editor.putString("userName",
+                ((EditText)findViewById(R.id.userName)).getText().toString());
+        editor.apply();
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
--- Qn 7 - Sharedprefs and Intents/MainActivity.java
+++ Qn 7 - Sharedprefs and Intents/MainActivity.java
@@ -1,82 +1,31 @@
 package com.example.qn7_sharedprefsandintents;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    private EditText editTextUsername;
-    private EditText editTextPassword;
-    private Button btnLogin;
-    private SharedPreferences preferences;
-
+    SharedPreferences preferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        editTextUsername = findViewById(R.id.editTextUsername);
-        editTextPassword = findViewById(R.id.editTextPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        preferences = getPreferences(MODE_PRIVATE);
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                login();
-            }
-        });
-
-        // Check if the user is already logged in
-        if (isLoggedIn()) {
-            startHomeActivity();
+        preferences = getSharedPreferences("creds", MODE_PRIVATE);
+        String userName = preferences.getString("userName", "");
+        if(userName.equals("")) {
+            startActivity(new Intent(this, LoginScreen.class));
         }
+        ((TextView)findViewById(R.id.userNameDisplay)).setText("Logged in as:"+
+                userName);
     }
-
-    private void login() {
-        String username = editTextUsername.getText().toString().trim();
-        String password = editTextPassword.getText().toString().trim();
-
-        // Replace "admin" and "password" with your desired default values
-        String defaultUsername = "admin";
-        String defaultPassword = "password";
-
-        if (username.equals(defaultUsername) && password.equals(defaultPassword)) {
-            // Save login status to SharedPreferences
-            saveLoginStatus(true);
-
-            // Start HomeActivity
-            startHomeActivity();
-        } else {
-            showToast("Invalid credentials");
-        }
-    }
-
-    private void startHomeActivity() {
-        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-        startActivity(intent);
-        finish(); // finish the current activity
-    }
-
-    private void showToast(String message) {
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-    }
-
-    private boolean isLoggedIn() {
-        // Check the login status from SharedPreferences
-        return preferences.getBoolean("isLoggedIn", false);
-    }
-
-    private void saveLoginStatus(boolean isLoggedIn) {
-        // Save the login status to SharedPreferences
+    public void logout(View view){
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putBoolean("isLoggedIn", isLoggedIn);
+        editor.putString("userName", "");
         editor.apply();
+        startActivity(new Intent(this, LoginScreen.class));
     }
 }
